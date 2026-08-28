c473b1b0d9a5efaf521766050becc618947d4a58
Aarush Singh <a882sing@uwaterloo.ca>
2026-08-28 01:36:32 -0400

    Add: matrix-diagonal-sum - submission-3

---
+++ Data Structures & Algorithms/matrix-diagonal-sum/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        if len(mat)%2 == 0:
+            s = 0
+            c = 0
+            for i in range(len(mat)):
+                s = s + i[c]
+                c += 1
+            
+            c = -1
+            for j in range(-1,-len(mat-1,-1)):
+                s = s+ i[c]
+                c = c -1
+        else:
+            s = 0
+            c = 0
+            for i in range(len(mat)):
+                if i == len(mat) %2 + 1:
+                    continue
+                else:
+                    s = s + i[c]
+                    c += 1
+            
+            c = -1
+            for j in range(-1,-len(mat-1,-1)):
+                s = s+ i[c]
+                c = c -1
+
+        return s
+
+
+        
+        
+
+            
+            
+
+        
